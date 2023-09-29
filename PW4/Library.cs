cf0d932b48ea0a8c3f2df1ccb1f61db61ef01d53
Amir Zhumagaliev <leasholea@gmail.com>
2023-09-29 19:52:19 +0600

    Добавьте файлы проекта.

---
+++ PW4/Library.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Year { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Author}, Year: {Year}";
+    }
+}
+
+class LibraryRealization
+{
+    private List<Book> books;
+
+    public LibraryRealization()
+    {
+        books = new List<Book>();
+    }
+
+    public void AddBook(Book book)
+    {
+        books.Add(book);
+    }
+    public void RemoveBook(Book book)
+    {
+        books.Remove(book);
+    }
+
+    public List<Book> SearchByAuthor(string author)
+    {
+        return books.Where(book => book.Author.Equals(author, StringComparison.OrdinalIgnoreCase)).ToList();
+    }
+    public List<Book> SearchByYear(int year)
+    {
+        return books.Where(book => book.Year == year).ToList();
+    }
+    public void SortByTitle()
+    {
+        books = books.OrderBy(book => book.Title).ToList();
+    }
+    public void SortByAuthor()
+    {
+        books = books.OrderBy(book => book.Author).ToList();
+    }
+
+    public void SortByYear()
+    {
+        books = books.OrderBy(book => book.Year).ToList();
+    }
+    public void DisplayBooks()
+    {
+        Console.WriteLine("Books in the Library:");
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
+}
+
+class Library
+{
+    static void Main()
+    {
+        LibraryRealization library = new LibraryRealization();
+        library.AddBook(new Book { Title = "Book1", Author = "Author1", Year = 2020 });
+        library.AddBook(new Book { Title = "Book2", Author = "Author2", Year = 2018 });
+        library.AddBook(new Book { Title = "Book3", Author = "Author1", Year = 2019 });
+
+        library.DisplayBooks();
+        Console.WriteLine("\nBooks by Author1:");
+        List<Book> author1Books = library.SearchByAuthor("Author1");
+        foreach (var book in author1Books)
+        {
+            Console.WriteLine(book);
+        }
+
+        library.SortByAuthor();
+        Console.WriteLine("\nBooks sorted by Author:");
+        library.DisplayBooks();
+    }
+}
---
+++ PW4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+struct Student
+{
+    public string Name;
+    public string GroupNumber;
+    public int[] Grades;
+
+    public double GetAverageGrade()
+    {
+        return Grades.Average();
+    }
+
+    public bool HasExcellentGrades()
+    {
+        return Grades.All(grade => grade == 4 || grade == 5);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student[] students = new Student[10];
+
+        students[0] = new Student { Name = "Иванов И.И.", GroupNumber = "Группа 1", Grades = new int[] { 5, 4, 5, 4, 5 } };
+        students[1] = new Student { Name = "Петров П.П.", GroupNumber = "Группа 2", Grades = new int[] { 4, 4, 3, 5, 4 } };
+        students[2] = new Student { Name = "Сидоров С.С.", GroupNumber = "Группа 1", Grades = new int[] { 3, 3, 4, 4, 5 } };
+        students[3] = new Student { Name = "Козлов К.К.", GroupNumber = "Группа 2", Grades = new int[] { 5, 5, 5, 5, 5 } };
+        students[4] = new Student { Name = "Михайлов М.М.", GroupNumber = "Группа 3", Grades = new int[] { 4, 4, 4, 4, 4 } };
+        students[5] = new Student { Name = "Александров А.А.", GroupNumber = "Группа 3", Grades = new int[] { 3, 3, 3, 3, 3 } };
+        students[6] = new Student { Name = "Егоров Е.Е.", GroupNumber = "Группа 1", Grades = new int[] { 5, 4, 5, 5, 4 } };
+        students[7] = new Student { Name = "Николаев Н.Н.", GroupNumber = "Группа 2", Grades = new int[] { 3, 5, 4, 4, 5 } };
+        students[8] = new Student { Name = "Дмитриев Д.Д.", GroupNumber = "Группа 3", Grades = new int[] { 4, 4, 3, 4, 5 } };
+        students[9] = new Student { Name = "Андреев А.А.", GroupNumber = "Группа 2", Grades = new int[] { 5, 5, 5, 4, 5 } };
+
+        var sortedStudents = students.OrderBy(student => student.GetAverageGrade()).ToArray();
+
+        Console.WriteLine("Студенты, отсортированные по среднему баллу:");
+        foreach (var student in sortedStudents)
+        {
+            Console.WriteLine($"{student.Name}, Средний балл: {student.GetAverageGrade()}");
+        }
+
+        Console.WriteLine("\nСтуденты с оценками 4 или 5 во всех предметах:");
+        foreach (var student in students)
+        {
+            if (student.HasExcellentGrades())
+            {
+                Console.WriteLine($"{student.Name}, Группа: {student.GroupNumber}");
+            }
+        }
+    }
+}
+}
